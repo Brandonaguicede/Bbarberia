2a28b44d515ba6e4f022c005284bc1b24d2a31e1
Brandon Aguiire Cedeño <128259254+Brandonaguicede@users.noreply.github.com>
2026-03-30 22:43:03 -0600

    Remove requireAuth middleware from available-slots route

--- src/routes/appointments.js
+++ src/routes/appointments.js
@@ -4,7 +4,7 @@ const ctrl = require('../controllers/appointmentsController');
 const { requireAuth, requireAdmin } = require('../middleware/auth');
 
 router.get('/stats', requireAdmin, ctrl.getStats);
-router.get('/available-slots', requireAuth, ctrl.getAvailableSlots);
+router.get('/available-slots', ctrl.getAvailableSlots);
 router.get('/', requireAuth, ctrl.getAll);
 router.get('/:id', requireAuth, ctrl.getById);
 router.post('/', requireAuth, ctrl.create);
